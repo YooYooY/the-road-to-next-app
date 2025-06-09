762c554a54a7633d51a6be2191e23fd6097637b5
chenwl <13751840799@163.com>
2025-06-09 20:17:53 +0800

    ✨ feat: link-components

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,3 +1,5 @@
+import { initialTickets } from "@/data";
+
 type TicketPageProps = {
   params: {
     ticketId: string;
@@ -5,7 +7,21 @@ type TicketPageProps = {
 };
 
 const TicketPage = ({ params }: TicketPageProps) => {
-  return <h2 className="text-lg">TicketPage: {params.ticketId}</h2>;
+  
+  const ticket = initialTickets.find((ticket) => {
+    return ticket.id === params.ticketId;
+  });
+  
+  if(!ticket) {
+    return <h2 className="text-lg">Ticket not found</h2>;
+  }
+  
+  return (
+    <div>
+      <h2 className="text-lg">{ticket.title}</h2>
+      <p className="text-sm">{ticket.content}</p>
+    </div>
+  )
 };
 
 export default TicketPage;
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,22 @@
+import { initialTickets } from "@/data";
+import Link from "next/link";
+
 const TicketsPage = () => {
-  return <h2 className="text-lg">TicketsPage</h2>;
+  
+  return (
+    <div>
+      {
+        initialTickets.map((ticket) => {
+          return (
+            <div key={ticket.id}>
+              <h2 className="text-lg">{ticket.title}</h2>
+              <Link href={`/tickets/${ticket.id}`} className="text-sm underline"> View</Link>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 };
 
 export default TicketsPage;
