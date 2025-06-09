252a153053a6c6f048488d8b7842799d6e297b16
chenwl <13751840799@163.com>
2025-06-09 20:21:29 +0800

    feat: const path

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,4 +1,5 @@
 import { initialTickets } from "@/data";
+import { ticketPath } from "@/paths";
 import Link from "next/link";
 
 const TicketsPage = () => {
@@ -10,7 +11,7 @@ const TicketsPage = () => {
           return (
             <div key={ticket.id}>
               <h2 className="text-lg">{ticket.title}</h2>
-              <Link href={`/tickets/${ticket.id}`} className="text-sm underline"> View</Link>
+              <Link href={ticketPath(ticket.id)} className="text-sm underline"> View</Link>
             </div>
           )
         })
