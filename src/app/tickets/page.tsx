5732fa6fe9d2d1b5cd0ad5c8b667f394d186d2f4
chenwl <13751840799@163.com>
2025-06-12 15:51:39 +0800

    feat: fetch data in client

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,13 +1,35 @@
-import { initialTickets } from '@/data'
+'use client'
+
 import { Heading } from '@/components/heading'
 import { TicketItem } from '@/features/ticket/components/ticket-item'
+import { useEffect, useState } from 'react'
+import { Ticket } from '@/features/ticket/types'
+import { getTickets } from '@/features/ticket/queries/get-tickets'
 
 const TicketsPage = () => {
+  const [tickets, setTickets] = useState<Ticket[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      setIsLoading(true)
+      const result = await getTickets()
+      setTickets(result)
+      setIsLoading(false)
+    }
+
+    fetchTickets()
+  }, [])
+
+  if (isLoading) {
+    return <div className='flex justify-center'>Loading...</div>
+  }
+
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
       <div className="flex-1 flex flex-col gap-y-4 items-center animate-fade-in-from-top">
-        {initialTickets.map((ticket) => (
+        {tickets.map((ticket) => (
           <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
