7f1341175707f65909efea0e298709c7c588ac8e
chenwl <13751840799@163.com>
2025-06-12 17:50:30 +0800

    feat: server components streaming

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,21 +1,14 @@
 import { Heading } from '@/components/heading'
-import { TicketItem } from '@/features/ticket/components/ticket-item'
-import { useEffect, useState } from 'react'
-import { Ticket } from '@/features/ticket/types'
-import { getTickets } from '@/features/ticket/queries/get-tickets'
+import { TicketList } from '@/features/ticket/components/ticket-list'
+import { Suspense } from 'react'
 
 const TicketsPage = async () => {
-  
-  const tickets = await getTickets()
-
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
-      <div className="flex-1 flex flex-col gap-y-4 items-center animate-fade-in-from-top">
-        {tickets.map((ticket) => (
-          <TicketItem key={ticket.id} ticket={ticket} />
-        ))}
-      </div>
+      <Suspense>
+        <TicketList />
+      </Suspense>
     </div>
   )
 }
