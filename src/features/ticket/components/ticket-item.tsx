370651583b0dd6a669d14e7dbd42ff57c0936333
YooYooY <13751840799@163.com>
2025-06-16 21:57:25 +0800

    chore: use orm generated types

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,3 +1,4 @@
+import { Ticket } from '@prisma/client'
 import { LucideArrowUpRightFromSquare } from 'lucide-react'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
@@ -5,7 +6,6 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { cn } from '@/lib/utils'
 import { ticketPath } from '@/paths'
 import { TICKET_ICONS } from '../constants'
-import { Ticket } from '../types'
 
 type TicketItemProps = {
   ticket: Ticket
@@ -20,7 +20,7 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
       </Link>
     </Button>
   )
-  
+
   return (
     <div
       className={cn('w-full flex gap-x-1', {
--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,5 +1,6 @@
+import { Ticket } from '@prisma/client'
 import { prisma } from '@/lib/prisma'
-import { Ticket } from '../types'
+
 
 export const getTicket = async (id: string): Promise<Ticket | null> => {
   return await prisma.ticket.findUnique({
