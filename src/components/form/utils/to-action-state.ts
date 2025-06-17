c834ca89a9e9273e71375484cefdb671e95ee853
YooYooY <13751840799@163.com>
2025-06-17 16:51:32 +0800

    feat: premature optimization

--- src/components/form/utils/to-action-state.ts
+++ src/components/form/utils/to-action-state.ts
@@ -1,21 +1,37 @@
 import { ZodError } from 'zod'
 
 export type ActionState = {
+  status: 'IDLE' | 'SUCCESS' | 'ERROR'
   message: string
+  fieldErrors: Record<string, string[] | undefined>
+  timestamp: number
+}
+
+export const EMPTY_ACTION_STATE: ActionState = {
+  status: 'IDLE',
+  message: '',
+  fieldErrors: {},
+  timestamp: Date.now(),
 }
 
 export const fromErrorToActionState = (error: unknown): ActionState => {
   // if validation error with Zod, return first error message
   if (error instanceof ZodError) {
     return {
+      status: 'ERROR',
+      fieldErrors: error.flatten().fieldErrors,
+      timestamp: Date.now(),
       message: error.errors[0].message,
     }
   }
-  
+
   // if another error, return Error message
   // e.g database error
   if (error instanceof Error) {
     return {
+      status: 'ERROR',
+      timestamp: Date.now(),
+      fieldErrors: {},
       message: error.message,
     }
   }
@@ -23,6 +39,19 @@ export const fromErrorToActionState = (error: unknown): ActionState => {
   // if not an error instance but something else crashed
   // return generic error message
   return {
+    status: 'ERROR',
+    timestamp: Date.now(),
+    fieldErrors: {},
     message: 'An unknown error occurred',
+  }
+}
+
+export const toActionState = (status: ActionState['status'], message: string): ActionState => {
+  return {
+    ...EMPTY_ACTION_STATE,
+    status,
+    message,
+    fieldErrors: {},
+    timestamp: Date.now(),
   }
 }
