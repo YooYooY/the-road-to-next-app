f49f67ba28b8514a7d0e2b43aed2b1b6b432ddf7
YooYooY <13751840799@163.com>
2025-07-18 23:27:49 +0800

    fix: interplay interactions

--- src/features/ticket/components/ticket-pagination.tsx
+++ src/features/ticket/components/ticket-pagination.tsx
@@ -1,7 +1,8 @@
 'use client'
-import { useQueryStates } from 'nuqs'
-import { paginationOtions, paginationParser } from '../search-params'
+import { useQueryState, useQueryStates } from 'nuqs'
+import { paginationOtions, paginationParser, searchParser } from '../search-params'
 import Pagination from '@/components/pagination'
+import { useEffect, useRef } from 'react'
 
 type TicketPaginationProps = {
   paginatedTicketsMetadata: {
@@ -12,6 +13,18 @@ type TicketPaginationProps = {
 
 const TicketPagination = ({ paginatedTicketsMetadata }: TicketPaginationProps) => {
   const [pagination, setPagination] = useQueryStates(paginationParser, paginationOtions)
+  const [search] = useQueryState('search', searchParser)
+  const prevSearch = useRef(search)
+
+  useEffect(() => {
+    if (search === prevSearch.current) return
+    prevSearch.current = search
+
+    setPagination({
+      ...pagination,
+      page: 0,
+    })
+  }, [search, pagination, setPagination])
 
   return <Pagination pagination={pagination} onPagination={setPagination} paginatedTicketsMetadata={paginatedTicketsMetadata} />
 }
