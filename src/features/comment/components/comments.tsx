d72a8af1878ba5a4712af9f6dd4e0baf462efbb9
YooYooY <13751840799@163.com>
2025-07-30 18:48:10 +0800

    feat: create comment

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -1,19 +1,29 @@
-import { getComments } from "../queries/get-comments"
-import CommentItem from "./comment-item"
+import { CardCompact } from '@/components/card-compact'
+import { getComments } from '../queries/get-comments'
+import CommentCreateForm from './comment-create-form'
+import CommentItem from './comment-item'
 
 type CommentProps = {
   ticketId: string
 }
 
-const Comments = async ({ticketId}:CommentProps) => {
+const Comments = async ({ ticketId }: CommentProps) => {
   const comments = await getComments(ticketId)
-  
+
   return (
-    <div className="flex flex-col gap-y-2 mr-8">
-      {comments.map((comment) => (
-        <CommentItem key={comment.id} comment={comment} />
-      ))}
-    </div>
+    <>
+      <CardCompact
+        title="Create Comment"
+        description="A new comment will be created"
+        className='mr-8'
+        content={<CommentCreateForm ticketId={ticketId} />}
+      />
+      <div className="flex flex-col gap-y-2 mr-8">
+        {comments.map((comment) => (
+          <CommentItem key={comment.id} comment={comment} />
+        ))}
+      </div>
+    </>
   )
 }
 
